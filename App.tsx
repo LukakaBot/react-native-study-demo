fcf163ba84f5a60b8f1983d15099ac1b3cbfe989
Lokaka <monishine_code@163.com>
2024-10-19 21:59:56 +0800

    feat: 学习 Platform

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, SafeAreaView, Button, Alert } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Button,
+  Alert,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
-  const handlePress = () => {
-    console.log("pressed");
-  };
-
   return (
     <SafeAreaView style={styles.container}>
       <Button
-        color={"orange"}
-        title="Press Me"
+        title="Click Me"
         onPress={() =>
           Alert.prompt("My Title", "My message", (text) => console.log(text))
         }
       />
-      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
@@ -24,7 +24,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
