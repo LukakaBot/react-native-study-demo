757e9d7809f1faadfb0b5de2e643bf13b214793b
Lukaka <monishine_code@163.com>
2024-10-24 21:24:34 +0800

    feat: 学习 flexDirection

--- App.tsx
+++ App.tsx
@@ -2,10 +2,20 @@ import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "tomato", flex: 1 }}>
-      <View style={{ backgroundColor: "dodgerblue", flex: 2 }}></View>
-      <View style={{ backgroundColor: "gold", flex: 1 }}></View>
-      <View style={{ backgroundColor: "tomato", flex: 1 }}></View>
+    <View
+      style={{
+        backgroundColor: "white",
+        flex: 1,
+        flexDirection: "row",
+      }}
+    >
+      <View
+        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
+      ></View>
+      <View style={{ backgroundColor: "gold", width: 100, height: 100 }}></View>
+      <View
+        style={{ backgroundColor: "tomato", width: 100, height: 100 }}
+      ></View>
     </View>
   );
 }
