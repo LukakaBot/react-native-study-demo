dc29f93633f52c4faa8e7f58fa438050058bc9f3
Lukaka <monishine_code@163.com>
2024-10-24 21:57:00 +0800

    feat: 学习 flexBasis、flexGrow 和 flexShrink

--- App.tsx
+++ App.tsx
@@ -9,20 +9,22 @@ export default function App() {
         flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
-        alignContent: "center",
-        flexWrap: "wrap",
       }}
     >
       <View
-        style={{ backgroundColor: "dodgerblue", width: 100, height: 300 }}
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 400,
+          // flexBasis: 100,
+          // flexGrow: 1,
+          flexShrink: 1,
+          // flex: -1,
+          height: 100,
+        }}
       ></View>
       <View style={{ backgroundColor: "gold", width: 100, height: 100 }}></View>
       <View
         style={{ backgroundColor: "tomato", width: 100, height: 100 }}
-      ></View>
-      <View style={{ backgroundColor: "grey", width: 100, height: 100 }}></View>
-      <View
-        style={{ backgroundColor: "greenyellow", width: 100, height: 100 }}
       ></View>
     </View>
   );
